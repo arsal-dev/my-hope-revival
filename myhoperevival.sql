93f2b3b749015a41beaeb42c45f5b8e340540581
arsal-dev <arsalrajput173@gmail.com>
2022-06-21 17:56:38 +0500

    database update

--- myhoperevival.sql
+++ myhoperevival.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2022 at 06:50 PM
+-- Generation Time: Jun 21, 2022 at 02:55 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -20,6 +20,85 @@ SET time_zone = "+00:00";
 --
 -- Database: `myhoperevival`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `chat_id` int(11) NOT NULL,
+  `req_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_req`
+--
+
+CREATE TABLE `chat_req` (
+  `chat_req_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `accepted` tinyint(4) NOT NULL DEFAULT 0,
+  `chat_start_time` datetime NOT NULL,
+  `chat_end_time` datetime NOT NULL,
+  `chat_ended` tinyint(4) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `birthday` date NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`id`, `name`, `email`, `birthday`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'hostcry.com', 'demo@financialplugins.com', '2022-06-20', '$2y$10$QVMynIVPbYT5K74m/juzW.N6BPujUswrhpk9662CC8xe8Wa29PkTu', '2022-06-20 09:59:41', '2022-06-20 09:59:41'),
+(2, 'hostcry.com', 'Arsalrajput173@gmail.com', '2022-06-13', '$2y$10$vlXgihE.aUnyhaWpEcm/6O71wP4C8NKZaQRRz0bl8xjbWpOn280ay', '2022-06-20 10:29:58', '2022-06-20 10:29:58'),
+(3, 'hostcry.com', 'test@12.com', '2022-06-17', '$2y$10$Vqu.NNYs4ZgyVqkJdoYyp.uVbS9g0mtCvECMxdXqjl2ZHpuiC5EXG', '2022-06-20 10:31:15', '2022-06-20 10:31:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `id` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `image_desc` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `gallery`
+--
+
+INSERT INTO `gallery` (`id`, `image`, `image_desc`, `created_at`, `updated_at`) VALUES
+(3, '165538667120184342262ab322e8cd98.png', 'test image', '2022-06-16 13:37:50', '2022-06-16 13:37:50'),
+(4, '1655386681162191708762ab32390ae98.png', 'test image 2', '2022-06-16 13:38:01', '2022-06-16 13:38:01');
 
 -- --------------------------------------------------------
 
@@ -48,6 +127,30 @@ INSERT INTO `users` (`id`, `username`, `password`, `role`, `created_at`, `update
 --
 
 --
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`chat_id`);
+
+--
+-- Indexes for table `chat_req`
+--
+ALTER TABLE `chat_req`
+  ADD PRIMARY KEY (`chat_req_id`);
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -56,6 +159,30 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `chat_req`
+--
+ALTER TABLE `chat_req`
+  MODIFY `chat_req_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
